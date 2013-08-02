773aa9c2d640a3cc5a2f75d87f2256027407cdde
alok-sm <alok.shankar.m@gmail.com>
2013-08-02 17:37:09 +0530

    my commit

--- GitAdd.sh
+++ GitAdd.sh
@@ -1,20 +1,38 @@
 mv ./Gemfile ./gemtemp/Gemfile
 mv ./config/database.yml ./config/dbtemp/database.yml
 
+echo "moved sqlite3 files to temporary location"
+
 mv ./gem/Gemfile ./Gemfile
 mv ./config/db/database.yml ./config/database.yml 
 
+echo "moved PG files to location"
+
 bundle install
 rake db:migrate
+
+echo "Installed PG"
 
 git add .
 git commit -m "my commit"
 git push heroku master
 
+echo "Pushed to Heroku"
+
 mv ./Gemfile ./gem/Gemfile
 mv ./config/database.yml ./config/db/database.yml
+
+echo "moved PG files back"
 
 mv ./gemtemp/Gemfile ./Gemfile
 mv ./config/dbtemp/database.yml ./config/database.yml
 
+echo "moved sqlite3 to original location"
+
 bundle install
+
+echo "Installed Sqlite3"
+
+echo "Mission Acomplished"
+
+echo "Have a good day!"
